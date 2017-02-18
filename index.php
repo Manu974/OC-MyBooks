b35bc5649cda7dac25f5252aaa7e206e07adce84
DIJOUX <emmanuel.dijoux14@gmail.com>
2017-02-18 10:56:10 +0100

    isolation de l'acces aux donnees, MVC

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $bdd = new PDO('mysql:host=localhost;dbname=mybooks;charset=utf8', 'mybooks_user', 'secret');
-    $books = $bdd->query('select * from book order by book_id desc');
 
+require 'model.php';
+$books = getBooks();
 require 'view.php';
